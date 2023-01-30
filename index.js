82b8fb89237adaa3175650067c7629177c648654
Prajakta-collab <prajaktabadgujar2281@gmail.com>
2023-01-30 12:39:37 +0530

    deployment changes

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const connectToMongo = require("./db");
 const express = require("express");
 var cors = require("cors");
+require('dotenv').config()
 
 connectToMongo();
 const app = express();
-const port = 5001;
+const port = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cors());
